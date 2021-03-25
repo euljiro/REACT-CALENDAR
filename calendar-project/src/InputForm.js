24bb74fb5e48bf2df5df9ac0d49544cc2360a7d3
illy <ma_natsu03@naver.com>
2021-03-25 20:54:40 +0900

    리덕스 값 저장하기 성공

--- calendar-project/src/InputForm.js
+++ calendar-project/src/InputForm.js
@@ -9,10 +9,14 @@ import {
 } from '@material-ui/pickers';
 import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
-import { CardActions, TextField } from '@material-ui/core';
 import './style/Calendar.css'
-import {connect} from 'react-redux';
-
+import TextField from '@material-ui/core/TextField';
+import store from './redux/configStore';
+// 리덕스 스토어와 연결하기 위해 connect라는 친구를 호출할게요!
+import {connect, useDispatch} from 'react-redux';
+// 리덕스 모듈에서 (bucket 모듈에서) 액션 생성 함수 두개를 가져올게요!
+import {loadSchedule, createSchedule, loadScheduleFB, addScheduleFB} from './redux/modules/schedule';
+import { firestore } from './firebase';
 
 
 
@@ -22,7 +26,12 @@ const useStyles = makeStyles((theme) => ({
       width: '225px',
       margin: '8px 0',
     },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
   },
+  
 }));
 
 
@@ -33,53 +42,45 @@ const BasicTextFields = (props) =>  {
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
-    const [lists, setList] = useState();
-    const showRef = () => {
-      let list = [];
-      const new_item = textRef.current.value;
-      const new_date = selectedDate;
-      setList({lists: [...list, new_date, new_item]});    
-    };
-    console.log(lists)
+    const dispatch = useDispatch();
+    const addPlan = () => {
+      const new_date = dateTime;
+      if (new_date === "" ){
+        window.alert('날짜를 선택해 주세요');
+      } else if(textRef.current.value ==="" ){
+        window.alert('일정을 입력해 주세요');
+      } else{
+      const schedule = {'title':textRef.current.value, 'date':new_date, 'done':false};
+      dispatch(createSchedule(schedule));
+      {props.history.goBack('/')}
+      }
 
+    };
+    
+  
+    let dateTime='';
 
   return (
     <body className="input-body">
     <div className="input-style">
 
         <form className={classes.root} noValidate autoComplete="off">
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <Block>
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
+        <TextField
+          id="date"
           label="날짜를 선택해 주세요"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
+          type="date"
+          defaultValue="2021-03-24"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          onChange={(event)=>{
+            dateTime = event.target.value
           }}
         />
-        </Block>
-        <Block>
-        <KeyboardTimePicker
-          margin="normal"
-          id="time-picker"
-          label="시간을 선택해 주세요"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change time',
-          }}
-        />
-        </Block>
-    </MuiPickersUtilsProvider>
-            <TextField id="standard-basic" label="일정" inputRef={textRef}/>
+        <TextField id="standard-basic" label="일정" inputRef={textRef}/>
         </form>
-        <button className='list-btn' onClick={showRef} >
+        <button className='list-btn' onClick={addPlan} >
                     일정 추가하기
                 </button>
                 <button className='list-cancle-btn' 
@@ -88,7 +89,9 @@ const BasicTextFields = (props) =>  {
                 </button>
     </div>
     </body>
+    
   );
+  
 }
 
 
@@ -104,8 +107,5 @@ const InputForm = (props) => {
 
 }
 
-const Block= styled.div`
-    display:block
-`;
 
 export default withRouter(BasicTextFields);
